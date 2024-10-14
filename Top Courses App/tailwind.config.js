13a701aedbd85cd60a7f9b9cf3aefa11c4320d2b
Pranjal Rodge <78189904+pranjalr06@users.noreply.github.com>
2024-10-14 12:40:37 +0530

    Add files via upload

---
+++ Top Courses App/tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        bgDark: "#22223b",
+        bgDark2: "#4a4e69",
+      },
+    },
+  },
+  plugins: [],
+}
